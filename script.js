c35d62b799496e9863a97abe11a88751053fbe31
Texx45 <tcalderbank@hotmail.com>
2021-09-07 23:49:52 +0100

    completed level 100 functionality up to no4

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //You can edit ALL of the code here
+const renderContent = document.querySelector(".render-content");
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   //! Loop through data and retrieve required data
-  for (anything in allEpisodes) {
-    console.log(allEpisodes[anything]);
+  for (episode in allEpisodes) {
+    console.log(allEpisodes[episode]);
+
+    let { name, number, season, image, summary } = allEpisodes[episode];
+
+    number < 10 ? (number = `0${number}`) : (number = `${number}`);
+
+    season < 10 ? (season = `0${season}`) : (season = `${season}`);
+
+    const createCard = document.createElement("div");
+    createCard.innerHTML = `
+    <div class="movie">
+      <div class="movie-episode-info">Season: ${season} - Episode: ${number}</div>
+        <img
+          src="${image.medium}"
+          alt="${name}"
+        />
+     <div class="movie-info">
+          <h2>${name} 
+          </div>
+     <div class="summary">
+          <h2>Episode Summary</h2>
+          <p>${summary}</p>
+     </div>
+   </div>
+  
+    `;
+    renderContent.appendChild(createCard);
   }
   makePageForEpisodes(allEpisodes);
 }
