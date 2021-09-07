af5d49bc075ab10e0db41a2c370b3dd5f8374f93
Texx45 <tcalderbank@hotmail.com>
2021-09-07 21:09:55 +0100

    completed CSS cards

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
+  //! Loop through data and retrieve required data
+  for (anything in allEpisodes) {
+    console.log(allEpisodes[anything]);
+  }
   makePageForEpisodes(allEpisodes);
 }
 
