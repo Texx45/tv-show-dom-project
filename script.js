96da8aed0a79a5ab662ef394cf0931aee57d1beb
Texx45 <tcalderbank@hotmail.com>
2021-09-08 13:44:05 +0100

    Just finished filter function, change to lower case

--- script.js
+++ script.js
@@ -2,24 +2,43 @@
 const renderContent = document.querySelector(".render-content");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
+let allEpisodes = [];
 
 //! SEARCH BAR FUNCTIONALITY
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("input", (e) => {
   e.preventDefault();
   const searchEpisodes = search.value;
-  console.log(searchEpisodes); // returns value from search input
-  search.value = ""; // clears search value
+  filterSearchResults(allEpisodes, searchEpisodes);
 });
 
-//! SETUP FUNCTION TO RENDER INFO TO SCREEN
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  //? Loop through data and retrieve required data
-  for (episode in allEpisodes) {
-    console.log(allEpisodes[episode]);
+//! FILTER FUNCTION
+const filterSearchResults = (arrayOfEpisodes, searchInput) => {
+  const filteredEpisodes = arrayOfEpisodes.filter((episode) => {
+    return (
+      episode.summary.includes(searchInput) ||
+      episode.name.includes(searchInput)
+    );
+  });
+  console.log(filteredEpisodes);
 
-    let { name, number, season, image, summary } = allEpisodes[episode];
+  renderCard(filteredEpisodes);
+};
+
+//! SETUP FUNCTION
+
+function setup() {
+  allEpisodes = getAllEpisodes();
+  renderCard(allEpisodes);
+}
+
+//! RENDER FUNCTION (renders all cards to screen)
+const renderCard = (array) => {
+  renderContent.innerHTML = "";
+  array.forEach((episode) => {
+    // console.log(episode.name);
+
+    let { name, number, season, image, summary } = episode;
 
     number < 10 ? (number = `0${number}`) : (number = `${number}`);
 
@@ -27,26 +46,27 @@ function setup() {
 
     const createCard = document.createElement("div");
     createCard.innerHTML = `
-    <div class="movie">
-      <div class="movie-episode-info">Season: ${season} - Episode: ${number}</div>
-        <img
-          src="${image.medium}"
-          alt="${name}"
-        />
-     <div class="movie-info">
-          <h2>${name} 
-          </div>
-     <div class="summary">
-          <h2>Episode Summary</h2>
-          <p>${summary}</p>
-     </div>
-   </div>
-  
-    `;
+        <div class="movie">
+          <div class="movie-episode-info">Season: ${season} - Episode: ${number}</div>
+            <img
+              src="${image.medium}"
+              alt="${name}"
+            />
+         <div class="movie-info">
+              <h2>${name}
+              </div>
+         <div class="summary">
+              <h2>Episode Summary</h2>
+              <p>${summary}</p>
+         </div>
+       </div>
+
+        `;
     renderContent.appendChild(createCard);
-  }
-  makePageForEpisodes(allEpisodes);
-}
+
+    // makePageForEpisodes(allEpisodes);
+  });
+};
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
